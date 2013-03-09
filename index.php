5d58325353e3c07950d39ec4718b9c2eff3ab66a
Ali aGHasizadeh <me@aligh.pro>
2013-03-09 18:33:43 +0330

    better style for mob

--- index.php
+++ index.php
@@ -60,12 +60,22 @@
 					</a>
 				</div>
 
+				<div class="nightly left">
+					<a href="http://nightly.mozilla.org/">
+						<div class="icon"></div>
+						<h2>فایرفاکس شبانه</h2>
+						<p class="info">همین طور که از اسم مرورگر نشون میده، این مرورگر ورژن شبانه فایرفاکس هستش یا به عبارت دیگه این ورژن مخصوص توسعه دهنده ها و عاشقان تکنولوژیه</p>
+						<p class="version">آخرین ورژن : 21</p>
+						<p class="website">اطلاعات بیشتر</p>
+					</a>
+				</div>
+
 				<div class="firefox left">
 					<a href="http://www.firefox.com/">
 						<div class="icon"></div>
 						<h2>موزیلا فایرفاکس</h2>
 						<p class="info">مرورگری سریع و پرقدرت. فایرفاکس بر اساس امنیت بیشتر ساخته شده، با این مرورگر امنیت رو تجربه کنید.</p>
-						<p class="version">آخرین ورژن : 17</p>
+						<p class="version">آخرین ورژن : 18</p>
 						<p class="website">اطلاعات بیشتر</p>
 					</a>
 				</div>
@@ -75,7 +85,7 @@
 						<div class="icon"></div>
 						<h2>گوگل کروم</h2>
 						<p class="info">سریع ترین مرورگر موجود، پر قدرت، امن و محیط کاربری ساده. همین حالا امتحانش کنید.</p>
-						<p class="version">آخرین ورژن : 23</p>
+						<p class="version">آخرین ورژن : 25</p>
 						<p class="website">اطلاعات بیشتر</p>
 					</a>
 				</div>
@@ -93,24 +103,6 @@
 			</div>
 		</footer>
 	</section>
-
-	<script type="text/javascript">
-		//<![CDATA[
-		var owa_baseUrl = 'http://1dws.com/owa/';
-		var owa_cmds = owa_cmds || [];
-		owa_cmds.push(['setSiteId', 'e1758600144707ee91f139696d9f2c84']);
-		owa_cmds.push(['trackPageView']);
-		owa_cmds.push(['trackClicks']);
-		owa_cmds.push(['trackDomStream']);
-
-		(function() {
-			var _owa = document.createElement('script'); _owa.type = 'text/javascript'; _owa.async = true;
-			owa_baseUrl = ('https:' == document.location.protocol ? window.owa_baseSecUrl || owa_baseUrl.replace(/http:/, 'https:') : owa_baseUrl );
-			_owa.src = owa_baseUrl + 'modules/base/js/owa.tracker-combined-min.js';
-			var _owa_s = document.getElementsByTagName('script')[0]; _owa_s.parentNode.insertBefore(_owa, _owa_s);
-		}());
-		//]]>
-	</script>
-	
+	<script type="text/javascript" src="cordova-2.4.0.js"></script>
 </body>
 </html>
